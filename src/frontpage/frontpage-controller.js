168d69f75017f50182df318c43db22c8f47dc109
Oscar Nilsson <oscar@ondproduktion.se>
2026-04-02 18:10:51 +0200

    Adjusted file structure and added some placeholders and primitive styling.

---
+++ src/frontpage/frontpage-controller.js
@@ -0,0 +1,8 @@
+import { renderFrontpage } from './frontpage-view.js';
+
+export function initFrontpage() {
+    // Ladda funktioner etc som vi vill ha på startsidan här.
+
+    renderFrontpage();
+    console.log('Frontpage initialized');
+}
---
+++ src/frontpage/frontpage-view.js
@@ -0,0 +1,15 @@
+export function renderFrontpage() {
+    document.querySelector('#app').innerHTML = `
+        <h1>Workout App</h1>
+        <p>Welcome to the Workout App! Here you can find daily workout suggestions based on your workout history, preferences, and goals.</p>
+        <div id="suggestion-container">
+            <h2>Today's Workout Suggestion</h2>
+            Today's suggestion will be rendered here ... eventually.
+        </div>
+        <nav>
+            <a href='#/workouts'>Workouts</a>
+            <a href='#/exercises'>Exercises</a>
+            <a href='#/profile'>Profile</a>
+        </nav>
+    `;
+}
