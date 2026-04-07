3dd220d3c7d5aa73ad61449706594479e08d2448
Harald-Wallin <hara.w@hotmail.com>
2026-04-08 00:07:50 +0200

    init Profile JS, (not functional)

--- src/profile/profile-model.js
+++ src/profile/profile-model.js
@@ -1,1 +1,27 @@
-//API-anrop för 
+import { getUserProfile } from './profile-api.js';
+
+export async function fetchUserProfile() {
+    const data = await getUserProfile();
+
+    const user = data[0]; // vi har bara en user
+
+    // Sortera measurements (senaste först)
+    const measurements = user.user_measurements.sort((a, b) =>
+        new Date(b.measured_at) - new Date(a.measured_at)
+    );
+
+    const latestMeasurement = measurements[0];
+
+    return {
+        id: user.id,
+        username: user.username,
+        mail: user.mail,
+        fname: user.fname,
+        lname: user.lname,
+        birthdate: user.birthdate, // om du har den
+
+        measured_at: latestMeasurement?.measured_at,
+        height_cm: latestMeasurement?.height_cm,
+        weight_kg: latestMeasurement?.weight_kg
+    };
+}
