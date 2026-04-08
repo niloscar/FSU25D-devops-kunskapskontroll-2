c689f01133c3abc24ebf95615f4f11c8e0d52938
NAKSI <d.nakshbandi@gmail.com>
2026-04-08 21:30:49 +0200

    fix bug

--- src/exercise/exercise-api.js
+++ src/exercise/exercise-api.js
@@ -1,6 +1,6 @@
 import { get } from '../services/api-service.js';
 
-export async function getExercisesByWorkout(workoutId = 1) {
+export async function getExercisesByWorkout() {
     const table = 'exercises';
     const query = {
         select: '*',
