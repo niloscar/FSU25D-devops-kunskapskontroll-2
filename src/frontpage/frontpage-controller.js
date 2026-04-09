a706cdd4c4115cf5cc4dc7043bd620cbda2acc2d
Oscar Nilsson <62339039+niloscar@users.noreply.github.com>
2026-04-09 17:01:08 +0200

    Remove unused import in frontpage-controller.js
    Removed unused import of 'isToday' from suggestion-model.js.

--- src/frontpage/frontpage-controller.js
+++ src/frontpage/frontpage-controller.js
@@ -1,6 +1,5 @@
 import { renderFrontpage } from './frontpage-view.js';
 import { generateSuggestion } from '../suggestion/suggestion-controller.js';
-import { isToday } from '../suggestion/suggestion-model.js';
 
 export function initFrontpage() {
     renderFrontpage();
